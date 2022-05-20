5be21dcd1ab53e44941b2e7bc9b7d8da0888475a
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-20 21:23:40 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
-public class Hello_java {
+public class Numbers {
     public static void main(String[] args) {
-        System.out.println("Hello Nazarii");
+
+        double pi = 3.141592653F;
+        double number = 2.7182818284590452D;
+
+        System.out.println("pi = " + pi + "\nnumber = " + number);
+
     }
 }
